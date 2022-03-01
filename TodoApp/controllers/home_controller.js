61674675034469e0303d2408985f5fbede008c6a
Amit Paikra <paikramit1999@gmail.com>
2022-03-01 23:48:18 +0530

    active addtask button

--- TodoApp/controllers/home_controller.js
+++ TodoApp/controllers/home_controller.js
@@ -1,3 +1,25 @@
+
+let tasks = [
+    "why not add a task?",
+    "Let's make a todoApp",
+    "Annual report submission deadline"
+];
+
 module.exports.home = function(req, res){
-    return res.render("home");
+    return res.render("home",{
+        task_list:tasks
+    });
 };
+
+
+module.exports.addTask = function(req, res){
+    tasks.push(req.query.user_description);
+    let chooseCat = req.query.choose_category;
+    let dueDate = req.query.user_date;
+
+    return res.redirect('back');
+};
+
+module.exports.deleteTask = function(req, res){
+    // do nothing
+}
--- TodoApp/routers/index.js
+++ TodoApp/routers/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller");
 
 router.get("/",homeController.home);
+router.get("/task_list", homeController.addTask);
+router.get("/delete_list", homeController.deleteTask);
 
 module.exports = router;
